835513b3fc0ace8f79bc4d911ad76228c918e0c3
Angel Goldsmith <angelgoldy13@yahoo.com>
2026-09-09 11:21:15 -0400

    Add header comment and introduction lines

--- lab0-hello/HelloWorld/Program.cs
+++ lab0-hello/HelloWorld/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*
+* Name: Angel Goldsmith
+* Course: CSCI 1250, Section 001
+* Assignment: Lab 0, Hello World
+* Date: September 9, 2026
+* Description: Prints a greeting and a short introduction to the console.
+*/
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is Angel Goldsmith.");
+Console.WriteLine("This is my first program at ETSU.");
